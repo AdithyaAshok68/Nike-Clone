e13f4a10685dcd33a9d6a6526bca89321fcd805c
Adithya Ashok <adithyaashok68@gmail.com>
2024-03-21 14:38:05 +0530

    Nike

---
+++ index.js
@@ -0,0 +1,19 @@
+import Hero from "./src/Sections/Hero";
+import PopularProducts from "./src/Sections/PopularProducts";
+import SuperQuality from "./src/Sections/SuperQuality";
+import Services from "./src/Sections/Services";
+import SpecialOffer from "./src/Sections/SpecialOffer";
+import CustomerReviews from "./src/Sections/CustomerReviews";
+import Subscribe from "./src/Sections/Subscribe";
+import Footer from "./src/Sections/Footer";
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    CustomerReviews,
+    Subscribe,
+    Footer
+} ;
